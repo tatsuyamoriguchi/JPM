28481b448a8f0cb118cd60707b5825c43a64f8b7
Brian Moriguchi <brian@beckos.com>
2021-10-31 13:34:07 -0700

    Initial Commit

---
+++ JPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JPM
+//
+//  Created by Tatsuya Moriguchi on 10/31/21.
+//  Copyright © 2021 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JPMTests/JPMTests.swift
@@ -0,0 +1,34 @@
+//
+//  JPMTests.swift
+//  JPMTests
+//
+//  Created by Tatsuya Moriguchi on 10/31/21.
+//  Copyright © 2021 Tatsuya Moriguchi. All rights reserved.
+//
+
+import XCTest
+@testable import JPM
+
+class JPMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
